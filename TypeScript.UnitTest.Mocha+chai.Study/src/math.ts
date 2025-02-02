71a30a98cb432733a6022343c73e2083371da412
解大劦 <9727005@qq.com>
2025-02-02 23:15:36 +0800

    修改文件夹名称

---
+++ TypeScript.UnitTest.Mocha+chai.Study/src/math.ts
@@ -0,0 +1,26 @@
+/*
+ * @Author       : Shandong Xiedali
+ * @Mail         : 9727005@qq.com
+ * @Date         : 2025/2/2 22:28:09
+ * @FilePath     : math.ts
+ * @Description  :
+ * Copyright (c) 2025 by Hiland & RainyTop, All Rights Reserved.
+ */
+
+export class Math{
+    static add(a: number, b: number): number {
+        return a + b;
+    }
+
+    static subtract(a: number, b: number): number {
+        return a - b;
+    }
+
+    static multiply(a: number, b: number): number {
+        return a * b;
+    }
+
+    static divide(a: number, b: number): number {
+        return a / b;
+    }
+};
---
+++ TypeScript.UnitTest.Mocha+chai.Study/tests/math.spec.ts
@@ -0,0 +1,41 @@
+/*
+ * @Author       : Shandong Xiedali
+ * @Mail         : 9727005@qq.com
+ * @Date         : 2025/2/2 22:32:49
+ * @FilePath     : math.spec.ts
+ * @Description  :
+ * Copyright (c) 2025 by Hiland & RainyTop, All Rights Reserved.
+ */
+import { expect } from 'chai';
+import { describe, it } from "mocha";
+
+import { Math } from '../src/math';
+
+export default {
+    describe: 'Math',
+};
+describe('math', () => {
+    it('add', () => {
+        expect(Math.add(1, 2)).equals(3);
+        expect(Math.add(0, 0)).equals(0);
+        expect(Math.add(-1, -2)).equals(-3);
+    });
+
+    it('minus', () => {
+        expect(Math.subtract(1, 2)).equals(-1);
+        expect(Math.subtract(0, 0)).equals(0);
+        expect(Math.subtract(-1, -2)).equals(1);
+    });
+
+    it('multiply', () => {
+        expect(Math.multiply(1, 2)).equals(2);
+        expect(Math.multiply(0, 0)).equals(0);
+        expect(Math.multiply(-1, -2)).equals(2);
+    });
+
+    it('divide', () => {
+        expect(Math.divide(1, 2)).equals(0.5);
+        expect(Math.divide(0, 0)).equals(NaN);
+        expect(Math.divide(-1, -2)).equals(0.5);
+    });
+});
