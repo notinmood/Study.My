76fbeea6869dc36dc215d96433aefafdedeaa180
山东解大劦 <9727005@qq.com>
2026-02-01 17:40:44 +0800

    feat(project): 初始化多模块Spring Boot项目结构
    - 创建根项目并配置多模块架构
    - 添加demo和utils两个子模块
    - 配置Maven Wrapper支持
    - 设置IDE相关配置文件和编码规范
    - 添加Git忽略规则和属性配置
    - 创建基础控制器和工具类
    - 配置应用名称和基本属性
    - 添加测试类和启动类

---
+++ MultiModuleProject.JAVA/utils/src/main/java/org/zerodegree/utils/Foo.java
@@ -0,0 +1,10 @@
+package org.zerodegree.utils;
+
+public class Foo {
+    private Foo() {
+    }
+
+    public static String sayHello(String someBody) {
+        return "Hello, " + someBody;
+    }
+}
